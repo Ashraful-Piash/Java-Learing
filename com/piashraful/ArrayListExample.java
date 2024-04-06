4b1e48141a361fbd2cc8f93913213324e41e7731
Ashraful Haque Piash <piashrafulhaque@gmail.com>
2024-04-06 14:49:22 +0600

    Arraylist implemented

--- com/piashraful/ArrayListExample.java
+++ com/piashraful/ArrayListExample.java
@@ -1,4 +1,14 @@
 package com.piashraful;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayListExample {
+    public static void main(String[] args) {
+        List<String > name = new ArrayList<>();
+        name.add("Piash");
+        name.add("jerin");
+        name.add("Aisha");
+        System.out.println(name);
+    }
 }
