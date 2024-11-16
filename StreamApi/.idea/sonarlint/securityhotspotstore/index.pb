baac37a3faa3009dab413fae99a5bd46d9a4a9d4
Ashraful Haque Piash <piashrafulhaque@gmail.com>
2024-11-16 18:58:17 +0600

    Extracted digits from a given number

---
+++ StreamApi/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,11 @@
+
+O
+src/com/piashraful/Project.java,7\0\70d05aecb65ce8a49b76e4ccda00b2d58d26d4dd
+P
+ src/com/piashraful/Employee.java,f\2\f213cc616eb69da9e051ae097b8ab485257e5628
+X
+(src/com/piashraful/EmployeeDataBase.java,a\d\ad54bf7e410b47c28e16730a527a62bf5a3b7f2a
+]
+-src/com/piashraful/Java8MethodCheatSheet.java,3\5\352425a8ea12dcfa6989bed6cfc2c64d9a32bdeb
+R
+"src/com/piashraful/Challenge1.java,6\8\6897b683068593c88bd9860eda18bf22f12ce8bc
--- StreamApi/src/com/piashraful/Java8MethodCheatSheet.java
+++ StreamApi/src/com/piashraful/Java8MethodCheatSheet.java
@@ -1,6 +1,7 @@
 package com.piashraful;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Java8MethodCheatSheet {
     public static void main(String[] args) {
@@ -10,5 +11,17 @@ public class Java8MethodCheatSheet {
 //            System.out.println(e.getName());
 //            System.out.println(e.getProjects());
             allEmployees.forEach(employee -> System.out.println(employee.getName() + " : " + employee.getSalary()));
-        }
+
+        List<Employee> employeeListOfDev = allEmployees.stream()
+                .filter(employee -> employee.getDept().equals("Development"))
+                .collect(Collectors.toList());
+        System.out.println("Employee list of devs: " + employeeListOfDev);
+
+        List<Employee> male = allEmployees.stream()
+                .filter(employee -> employee.getGender().equalsIgnoreCase("male"))
+                .collect(Collectors.toList());
+        System.out.println("List of male employee" +male);
+
+
+    }
 }
